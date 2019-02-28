16577b177bdb5a8b9fc34122debe85b7463f1f73
Ethanpho2002 <47428025+Ethanpho2002@users.noreply.github.com>
2019-02-27 21:31:52 -0500

    Update and rename Python IF statements to Python IF statements.py

---
+++ Python IF statements.py
@@ -0,0 +1,16 @@
+Question 34 code: 
+
+a=int(input("Please enter a number:"))
+
+if(a%2==0):
+  print("That is an even number")
+
+else:
+  print("That is an odd number")
+
+
+--------------------------------------------
+
+Question 35 code: 
+
+...
